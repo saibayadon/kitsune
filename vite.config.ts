26ea3e993ffd45bf4bd63b115a8e796896049ba6
Majin Bru <brunomilestone@gmail.com>
2025-12-03 20:57:22 +0100

    feat: update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [tailwindcss(), tsconfigPaths(), react()],
 });
